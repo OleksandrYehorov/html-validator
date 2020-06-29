2b28e2a776fd446ff428e57a76bd5847cb29acef
OleksandrYehorov <egorow.sasha@gmail.com>
2020-06-29 23:30:17 +0300

    Add html validator

---
+++ index.test.ts
@@ -0,0 +1,183 @@
+import { validateHtml } from './index';
+
+interface TestCase {
+  testName: string;
+  inputHtml: string;
+}
+
+const validInputCases: TestCase[] = [
+  {
+    testName: 'empty input',
+    inputHtml: '',
+  },
+  {
+    testName: 'open and close tags',
+    inputHtml: '<div></div>',
+  },
+  {
+    testName: 'self closing tags',
+    inputHtml: '<img/>',
+  },
+  {
+    testName: 'self text content',
+    inputHtml: '<span>text content</span>',
+  },
+  {
+    testName: 'white spaces',
+    inputHtml: `
+      <p 
+    
+      >
+      
+      </p   
+
+
+      >`,
+  },
+  {
+    testName: 'numbers in tag names',
+    inputHtml: `<h3></h3>`,
+  },
+  {
+    testName: 'attributes',
+    inputHtml: `
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/240px-JavaScript-logo.png"
+        width="240"
+        height="240"
+        alt="lorem"
+      />
+    `,
+  },
+  {
+    testName: 'invalid html in attributes values',
+    inputHtml: `
+      <img
+        src="<span><p></span></p>"
+        width="240"
+        height="240"
+        alt="lorem"
+      />
+    `,
+  },
+  {
+    testName: 'valid html in attributes values',
+    inputHtml: `
+      <img
+        src="<span>text content</span>"
+        width="240"
+        height="240"
+        alt="lorem"
+      />
+    `,
+  },
+  {
+    testName: 'nested markup',
+    inputHtml: ` 
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    `,
+  },
+  {
+    testName: 'comments',
+    inputHtml: `
+      <div>
+        <!-- <span>commented markup</span> -->
+        <div></div>
+        <!-- <img
+          src="/lib/images/footer-logo.jpg"
+          alt="logo"
+        /><div> -->
+      </div>
+    `,
+  },
+  {
+    testName: 'special chars in text content',
+    inputHtml: `<span>/!&lt;&%$}{#?</span>`,
+  },
+  {
+    testName: 'case insensitive tag names',
+    inputHtml: `<SPaN></Span>`,
+  },
+  {
+    testName: 'invalid html inside scripts',
+    inputHtml: `
+      <div>
+        <script>
+          const invalidHtmlString = '<div><span></div></span>';
+        </script>
+      </div>
+    `,
+  },
+  {
+    testName: 'valid html inside scripts',
+    inputHtml: `
+      <div>
+        <script>
+          const invalidHtmlString = '<div><span>text</span></div>';
+        </script>
+      </div>
+    `,
+  },
+  {
+    testName: 'special self closing tags without closing tag',
+    inputHtml: `
+      <div>
+        <input type="text">
+        <input type="text"/>
+      </div>
+    `,
+  },
+  {
+    testName: 'special self closing tags with closing tag',
+    inputHtml: `
+      <div>
+        <input type="text"></input>
+      </div>
+    `,
+  },
+  {
+    testName: 'text',
+    inputHtml: 'text content',
+  },
+];
+
+const invalidInputCases: TestCase[] = [
+  {
+    testName: 'unclosed tags',
+    inputHtml: '<div>',
+  },
+  {
+    testName: 'unopened tags',
+    inputHtml: '</div>',
+  },
+  {
+    testName: 'invalid tag sequences',
+    inputHtml: '<div><span></div></span>',
+  },
+];
+
+describe('validateHtml', () => {
+  describe('when input is valid html', () => {
+    describe('should handle', () => {
+      for (const { inputHtml: inputValue, testName } of validInputCases) {
+        test(testName, () => {
+          expect(validateHtml(inputValue)).toBe(true);
+        });
+      }
+    });
+  });
+
+  describe('when input is invalid html', () => {
+    describe('should handle', () => {
+      for (const { inputHtml, testName } of invalidInputCases) {
+        test(testName, () => {
+          expect(validateHtml(inputHtml)).toBe(false);
+        });
+      }
+    });
+  });
+});
---
+++ index.ts
@@ -0,0 +1,89 @@
+const selfClosingTags = new Set([
+  'area',
+  'base',
+  'br',
+  'col',
+  'command',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'keygen',
+  'link',
+  'menuitem',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+]);
+
+enum TokenType {
+  openTag = 'openTag',
+  closeTag = 'closeTag',
+  selfClosingTag = 'selfClosingTag',
+  comment = 'comment',
+  script = 'script',
+  style = 'style',
+}
+
+const matchers = {
+  [TokenType.openTag]: /<([a-z][a-z0-9-]*)(?:\s+[a-z][a-z0-9-]*?(?:="[\s\S]*?")?)*\s*>/gi,
+  [TokenType.closeTag]: /<\/([a-z][a-z0-9-]*)\s*>/gi,
+  [TokenType.selfClosingTag]: /<([a-z][a-z0-9-]*)(?:\s+[a-z][a-z0-9-]*?(?:="[\s\S]*?")?)*\s*\/>/gi,
+  [TokenType.comment]: /<!--[\s\S]*?-->/g,
+  [TokenType.script]: /<script[\s\S]*?<\/script\s*?>/g,
+  [TokenType.style]: /<style[\s\S]*?<\/style\s*?>/g,
+};
+
+function getNextToken(html: string, position: number) {
+  let token = null;
+
+  for (const [type, regExp] of Object.entries(matchers)) {
+    regExp.lastIndex = position;
+    const match = regExp.exec(html);
+    if (!match) continue;
+
+    if (token) {
+      if (match.index <= token.match.index) token = { type, match };
+    } else {
+      token = { type, match };
+    }
+  }
+
+  return token;
+}
+
+function* tokenize(html: string) {
+  let position = 0;
+  let token = null;
+
+  while ((token = getNextToken(html, position))) {
+    position = token.match.index + token.match[0].length;
+
+    if (token.type === TokenType.openTag || token.type === TokenType.closeTag) {
+      yield token;
+    }
+  }
+}
+
+export function validateHtml(inputHtml: string) {
+  const stack = [];
+
+  for (const { type, match } of tokenize(inputHtml.trim())) {
+    const tagName = match[1].toLowerCase();
+
+    if (selfClosingTags.has(tagName)) continue;
+
+    if (type === TokenType.openTag) {
+      stack.push(tagName);
+    } else if (stack.length > 0 && stack[stack.length - 1] === tagName) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+}
